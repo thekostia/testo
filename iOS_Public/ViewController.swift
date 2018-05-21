89d9d9e92ef7bba2be9826d6389afcf3fc822123
macmini <macmini@macminis-Mac-mini.local>
2018-05-21 13:59:12 +0300

    testo

---
+++ iOS_Public/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_Public
+//
+//  Created by Nick Kopilovskii on 18.05.2018.
+//  Copyright © 2018 Nick Kopilovskii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
